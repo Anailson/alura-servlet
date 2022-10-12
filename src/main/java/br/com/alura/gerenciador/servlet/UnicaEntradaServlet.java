3347410c03c295f2c2b20cbe1da83e5dd56a77f6
anailson ribeiro <arsantos315@gmail.com>
2022-10-12 15:45:37 -0300

    preparando modelo e view

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -27,43 +27,21 @@ public class UnicaEntradaServlet extends HttpServlet {
 
 		String nome;
 		try {
-			Class classe = Class.forName(paramAcao); // carrega a classe com o nome
-			Acao acao  = (Acao) classe.newInstance();
-			nome = acao.executa(request, response);
+			Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome 
+			Acao acao = (Acao) classe.newInstance();
+			nome = acao.executa(request,response);
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 			throw new ServletException(e);
 		}
-
+		
 		String[] tipoEEndereco = nome.split(":");
-		if (tipoEEndereco[0].equals("forward")) {
+		if(tipoEEndereco[0].equals("forward")) {
 			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
 			rd.forward(request, response);
 		} else {
 			response.sendRedirect(tipoEEndereco[1]);
-
 		}
 
-		// paramAcao.executa(req,res)
-
-		/*
-		 * String nome = null; if (paramAcao.equals("ListaEmpresas")) { ListaEmpresas
-		 * acao = new ListaEmpresas(); nome = acao.executa(request, response);
-		 * 
-		 * } else if (paramAcao.equals("RemoveEmpresa")) { RemoveEmpresa acao = new
-		 * RemoveEmpresa(); nome = acao.executa(request, response);
-		 * 
-		 * } else if (paramAcao.equals("MostraEmpresa")) { MostraEmpresa acao = new
-		 * MostraEmpresa(); nome = acao.executa(request, response);
-		 * 
-		 * } else if (paramAcao.equals("AlteraEmpresa")) { AlteraEmpresa acao = new
-		 * AlteraEmpresa(); nome = acao.executa(request, response);
-		 * 
-		 * } else if (paramAcao.equals("NovaEmpresa")) { NovaEmpresa acao = new
-		 * NovaEmpresa(); nome = acao.executa(request, response);
-		 * 
-		 * } else if (paramAcao.equals("NovaEmpresaForm")) { NovaEmpresaForm acao = new
-		 * NovaEmpresaForm(); nome = acao.executa(request, response); }
-		 */
-
+		
 	}
 }
